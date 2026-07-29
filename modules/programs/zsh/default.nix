ca91d058c588b1db3474f4492845c7c61e618afc
Ian Rayburn <ianrayburn9@gmail.com>
2026-07-29 17:53:10 -0400

    migrate zsh and p10k to nixos (no oh-my-zsh, pkgs through home man.)

---
+++ modules/programs/zsh/default.nix
@@ -0,0 +1,52 @@
+{ pkgs, ... }:
+{
+  programs.zsh = {
+    enable = true;
+    oh-my-zsh.enable = false;
+
+    enableCompletion = true;
+    autosuggestion = {
+      enable = true;
+      strategy = [ "history" "completion" ];
+    };
+    syntaxHighlighting.enable = true;
+
+    history = {
+      size = 10000;
+      save = 10000;
+      ignoreDups = true;
+      ignoreSpace = true;
+      expireDuplicatesFirst = true;
+      share = true;
+    };
+
+    sessionVariables = {
+      EDITOR = "nvim";
+      MANPAGER = "nvim +Man!";
+    };
+
+    shellAliases = {
+      la = "ls -lah";
+      gs = "git status";
+      ".." = "cd ..";
+      pub-ip = "curl ipinfo.io/ip";
+    };
+
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ];
+
+    initExtra = ''
+      autoload -Uz compinit
+      compinit -d ~/.cache/zsh/zcompdump
+
+      [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
+    '';
+  };
+
+  home.file.".p10k.zsh".source = ./p10k.zsh;
+}
