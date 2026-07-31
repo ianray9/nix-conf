ccf59678cd9c84a2aa64cc72b68891a11b3a8b64
Ian Rayburn <ianrayburn9@gmail.com>
2026-07-31 09:14:36 -0400

    zsh: add a few minor shell tools like zoxide and fzf

--- modules/programs/zsh/default.nix
+++ modules/programs/zsh/default.nix
@@ -13,70 +13,86 @@ let
       "| wl-copy";
 in
 {
-  programs.zsh = {
-    enable = true;
-    oh-my-zsh.enable = false;
-
-    enableCompletion = true;
-    autosuggestion = {
+  programs = {
+    fzf = {
       enable = true;
-      strategy = [ "history" "completion" ];
+      enableZshIntegration = true;
     };
-    syntaxHighlighting.enable = true;
-
-    autocd = true;
-
-    history = {
-      size = 10000;
-      save = 10000;
-      ignoreDups = true;
-      ignoreSpace = true;
-      expireDuplicatesFirst = true;
-      share = true;
+    
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+      options = [ "--cmd cd" ];
     };
 
-    defaultKeymap = "viins";
+    zsh = {
+      enable = true;
+      oh-my-zsh.enable = false;
 
-    sessionVariables = {
-      EDITOR = "nvim";
-      MANPAGER = "nvim +Man!";
+      enableCompletion = true;
+      autosuggestion = {
+        enable = true;
+        strategy = [ "history" "completion" ];
+      };
+      syntaxHighlighting.enable = true;
+
+      autocd = true;
+
+      history = {
+        size = 10000;
+        save = 10000;
+        ignoreDups = true;
+        ignoreSpace = true;
+        expireDuplicatesFirst = true;
+        share = true;
+      };
+
+      defaultKeymap = "viins";
+
+      sessionVariables = {
+        EDITOR = "nvim";
+        MANPAGER = "nvim +Man!";
+      };
+
+      shellAliases = {
+        update = UPDATE_CMD;
+
+        ls = "ls --color";
+        la = "ls --color -lah";
+        ".." = "cd ..";
+
+        zmv = "zmv -n";
+        zmv-e = "zmv";
+
+        gs = "git status";
+        pub-ip = "curl ipinfo.io/ip";
+        spf = "superfile";
+      };
+
+      shellGlobalAliases = {
+        nix-conf = "~/.config/nix";
+        nvim-conf = "~/.config/nix/modules/programs/nvim/config";
+        BUF = COPY_CMD;
+        NO_ERROR = "> /dev/null 2>&1";
+      };
+
+      plugins = [
+        {
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        }
+      ];
+
+      initContent = ''
+        autoload -Uz compinit
+        compinit -d ~/.cache/zsh/zcompdump
+
+        bindkey '^ ' autosuggest-accept
+
+        [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
+      '';
     };
-
-    shellAliases = {
-      update = UPDATE_CMD;
-
-      ls = "ls --color";
-      la = "ls --color -lah";
-      ".." = "cd ..";
-
-      gs = "git status";
-      pub-ip = "curl ipinfo.io/ip";
-      spf = "superfile";
-    };
-
-    shellGlobalAliases = {
-      nix-conf = "~/.config/nix";
-      nvim-conf = "~/.config/nix/modules/programs/nvim/config";
-      BUF = COPY_CMD;
-      NO_ERROR = "> /dev/null 2>&1";
-    };
-
-    plugins = [
-      {
-        name = "powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-    ];
-
-    initContent = ''
-      autoload -Uz compinit
-      compinit -d ~/.cache/zsh/zcompdump
-
-      bindkey '^ ' autosuggest-accept
-
-      [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
-    '';
   };
 
   home.file.".p10k.zsh".source = ./p10k.zsh;
